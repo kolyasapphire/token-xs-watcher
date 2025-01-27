e3292346ab94a62ee462461af6ab0afb2abe1714
kolyasapphire <a@ks.gg>
2025-01-27 23:35:06 +0000

    Formatting

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { CovalentClient, Chains } from "@covalenthq/client-sdk";
-import { load } from "@std/dotenv";
+import { Chains, CovalentClient } from '@covalenthq/client-sdk'
+import { load } from '@std/dotenv'
 
-await load({ export: true });
+await load({ export: true })
 
-const COVALENT_TOKEN = Deno.env.get("COVALENT_TOKEN");
-const ADDRESS = Deno.env.get("ADDRESS");
-const MIN_DIFFERENCE = Deno.env.get("MIN_DIFFERENCE"); // percentage
-const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
-const BOT_CHAT = Deno.env.get("BOT_CHAT");
+const COVALENT_TOKEN = Deno.env.get('COVALENT_TOKEN')
+const ADDRESS = Deno.env.get('ADDRESS')
+const MIN_DIFFERENCE = Deno.env.get('MIN_DIFFERENCE') // percentage
+const BOT_TOKEN = Deno.env.get('BOT_TOKEN')
+const BOT_CHAT = Deno.env.get('BOT_CHAT')
 
 const job = async () => {
-  console.time("job");
+  console.time('job')
 
   if (
     !COVALENT_TOKEN ||
@@ -19,8 +19,8 @@ const job = async () => {
     !BOT_TOKEN ||
     !BOT_CHAT
   ) {
-    console.error("Bad config");
-    return;
+    console.error('Bad config')
+    return
   }
 
   const sendMessage = async (
@@ -31,78 +31,77 @@ const job = async () => {
       chat_id: BOT_CHAT,
       text,
       ...options,
-    };
+    }
 
     const req = await fetch(
       `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(body),
       },
-    );
+    )
 
     if (!req.ok) {
-      console.error(await req.json());
+      console.error(await req.json())
     }
-  };
+  }
 
-  const client = new CovalentClient(COVALENT_TOKEN);
+  const client = new CovalentClient(COVALENT_TOKEN)
 
   const networks = [
     Chains.ETH_MAINNET,
     Chains.BASE_MAINNET,
     Chains.AVALANCHE_MAINNET,
-  ];
+  ]
 
   for (const network of networks) {
-    console.debug("Starting", network);
-    console.time(network);
+    console.debug('Starting', network)
+    console.time(network)
 
-    const res =
-      await client.BalanceService.getHistoricalPortfolioForWalletAddress(
-        network,
-        ADDRESS,
-        { quoteCurrency: "USD", days: 1 },
-      );
+    const res = await client.BalanceService.getHistoricalPortfolioForWalletAddress(
+      network,
+      ADDRESS,
+      { quoteCurrency: 'USD', days: 1 },
+    )
 
-    console.debug("Loaded", res.data.items.length, "tokens");
+    console.debug('Loaded', res.data.items.length, 'tokens')
 
     for (const token of res.data.items) {
       if (!token.holdings[0].quote_rate) {
         console.debug(
-          "Ignoring some random token (no quote)",
+          'Ignoring some random token (no quote)',
           token.contract_name,
           token.contract_address,
-        );
-        continue;
+        )
+        continue
       }
 
-      const from = token.holdings[0].open.quote;
-      const to = token.holdings[1].close.quote;
+      const from = token.holdings[0].open.quote
+      const to = token.holdings[1].close.quote
 
       if (to === 0) {
         console.debug(
-          "Ignoring zero balance token",
+          'Ignoring zero balance token',
           token.contract_name,
           token.contract_address,
-        );
-        continue;
+        )
+        continue
       }
 
       if (to < 60) {
         console.debug(
-          "Ignoring low balance token",
+          'Ignoring low balance token',
           token.contract_name,
           token.contract_address,
           token.holdings[1].close.quote,
-        );
-        continue;
+        )
+        continue
       }
 
-      const difference = (to / from - 1) * 100;
+      const difference = (to / from - 1) * 100
 
       if (difference >= Number.parseInt(MIN_DIFFERENCE)) {
         console.debug(
@@ -110,15 +109,17 @@ const job = async () => {
           `(${token.contract_ticker_symbol})`,
           `${Math.round(difference)}%`,
           token.contract_address,
-        );
+        )
         await sendMessage(
-          `${token.contract_name} (${token.contract_ticker_symbol}) increased ${Math.round(difference)}%`,
-        );
+          `${token.contract_name} (${token.contract_ticker_symbol}) increased ${
+            Math.round(difference)
+          }%`,
+        )
       }
     }
-    console.timeEnd(network);
+    console.timeEnd(network)
   }
-  console.timeEnd("job");
-};
+  console.timeEnd('job')
+}
 
-Deno.cron("job", "0 16 * * *", job);
+Deno.cron('job', '0 16 * * *', job)
